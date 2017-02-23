10973dab330b6086808f2e2f5a6f925966c624c2
Predrag Cokulov <predragcokulov@gmail.com>
2017-02-23 17:31:21 +0100

    chatterbot

--- iris.py
+++ iris.py
@@ -1,9 +1,24 @@
 from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
 
 iris = load_iris()
 
 X = iris.data
 y = iris.target
 
-print(X.shape)
-print(y.shape)
+knn = KNeighborsClassifier(n_neighbors=5)
+lr = LogisticRegression()
+
+knn.fit(X, y)
+
+label = knn.predict([[3, 5, 4, 2]])
+
+print("knn: ", end="")
+print(label)
+
+lr.fit(X, y)
+
+label = lr.predict([[3, 5, 4, 2]])
+print("lr: ", end="")
+print(label)
